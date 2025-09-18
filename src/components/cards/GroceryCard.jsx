f3e902785b60b373f57de37405412154f7b1e252
Andrea Moretti <andrea.moretti1704@gmail.com>
2025-09-18 09:37:21 +0200

    added function for add and function for remove

--- src/components/cards/GroceryCard.jsx
+++ src/components/cards/GroceryCard.jsx
@@ -1,40 +1,32 @@
 import { useCart } from "../../contexts/CartContext";
-import { useState } from "react";
 
 export default function GroceryCard({ Target }) {
-  const { cart, setCart } = useCart();
-  const addToCart = (fruit) => {
-    const isFruitInCart = cart.find(
-      (actualFruit) => actualFruit.name === fruit.name
-    );
-    if (isFruitInCart) {
-      setCart(
-        cart.map((target) =>
-          target.name === fruit.name
-            ? { ...target, quantity: target.quantity + 1 }
-            : target
-        )
-      );
-    } else {
-      setCart([...cart, { ...fruit, quantity: 1 }]);
-    }
-  };
+  const { addToCart, lessToCart, removeFromCart } = useCart();
 
-  const removeFromCart = (fruit) => {
-    setCart(cart.filter((toDeleteFruit) => toDeleteFruit.name !== fruit.name));
-  };
   return (
     <>
       <div className="card">
         <div className="card-body">
-          <p className="card-text">{Target.name}</p>
-          <p className="card-text">{Target.price} €</p>
+          <p className="card-text text-center">nome: {Target.name}</p>
+          <p className="card-text text-center ">prezzo: {Target.price} €</p>
           <div className="col-12 d-around my-3">
-            <button onClick={addToCart} className="btn btn-primary">
-              Aggiungi al carrello
+            <button
+              onClick={() => lessToCart(Target)}
+              className="btn btn-secondary "
+            >
+              ➖
             </button>
-            <button onClick={removeFromCart} className="btn btn-danger">
-              Rimuovi prodotto
+            <button
+              onClick={() => removeFromCart(Target)}
+              className="btn btn-danger "
+            >
+              🗑️
+            </button>
+            <button
+              onClick={() => addToCart(Target)}
+              className="btn btn-secondary "
+            >
+              ➕
             </button>
           </div>
         </div>
--- src/contexts/CartContext.jsx
+++ src/contexts/CartContext.jsx
@@ -9,10 +9,51 @@ function useCart() {
 function CartProvider({ children }) {
   const [cart, setCart] = useState([]);
 
+  const lessToCart = (fruit) => {
+    const isFruitInCart = cart.find(
+      (actualFruit) => actualFruit.name === fruit.name
+    );
+    if (isFruitInCart.quantity > 1) {
+      setCart(
+        cart.map((targetFruit) =>
+          targetFruit.name === fruit.name
+            ? { ...targetFruit, quantity: targetFruit.quantity - 1 }
+            : targetFruit
+        )
+      );
+    } else {
+      setCart(cart.filter((targetFruit) => targetFruit.name !== fruit.name));
+    }
+  };
+  const addToCart = (fruit) => {
+    const isFruitInCart = cart.find(
+      (actualFruit) => actualFruit.name === fruit.name
+    );
+
+    if (isFruitInCart) {
+      setCart(
+        cart.map((targetFruit) =>
+          targetFruit.name === fruit.name
+            ? { ...targetFruit, quantity: targetFruit.quantity + 1 }
+            : targetFruit
+        )
+      );
+    } else {
+      setCart([...cart, { ...fruit, quantity: 1 }]);
+    }
+  };
+
+  const removeFromCart = (fruit) => {
+    setCart(cart.filter((toDeleteFruit) => toDeleteFruit.name !== fruit.name));
+  };
+
   return (
-    <CartContext.Provider value={{ cart, setCart }}>
+    <CartContext.Provider
+      value={{ cart, lessToCart, addToCart, removeFromCart }}
+    >
       {children}
     </CartContext.Provider>
   );
 }
+
 export { useCart, CartProvider };
